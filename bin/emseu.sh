d9734a84e785cec98b1385546bf20766e9c4b02a
theus deka <me@theus.be>
2020-03-08 15:45:10 +0100

    irst commit

---
+++ bin/emseu.sh
@@ -0,0 +1,13 @@
+APP_ENV=dev
+APP_SECRET=7b19a4a6e37b9303e4f6bca1dc6691ed
+MAILER_URL=null://localhost
+ELASTICSEARCH_VERSION=5.6
+ELASTICSEARCH_CLUSTER=["http://elastic:changeme@elasticsearch1:9200"];
+STORAGE_FOLDER=./var/assets
+DB_DRIVER=mysql
+DB_USER=user
+DB_PASSWORD=password
+DB_PORT=3306
+DB_NAME=emseu
+TIKA_SERVER=http://tika:9998
+php --version
---
+++ readme.md
@@ -0,0 +1,10 @@
+#my dev environments
+
+##Expected local variables
+ - GIT_PATH: location of the git repository i.e.: /home/theus77/git
+ 
+##Launch environments
+````docker-compose up -d```` 
+
+##Enter in the dev php console 
+```docker exec -it php bash```
